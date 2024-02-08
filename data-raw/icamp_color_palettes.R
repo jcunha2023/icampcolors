c694f7802b04c52febff8b6c98d703da63e0d57c
Jason Cunha <jcunha1@scc-wi1.scc.bu.edu>
2024-02-08 13:17:03 -0500

    Updated icamp_color palette definitions to put all palettes in either discrete or sequential categories

--- data-raw/icamp_color_palettes.R
+++ data-raw/icamp_color_palettes.R
@@ -1,29 +1,20 @@
 library(palettes)
 
-#iCaMP Blue---------------------------------------------------------------------
+#Discrete Color Palettes--------------------------------------------------------
 
-icamp_blue_discrete <- pal_palette(
+icamp_discrete <- pal_palette(
 
-  icamp_blue = c("#1D0E8C") #rename so it isn't identical to fcn definition
+  icamp_blue = c("#1D0E8C"),
 
-)
+  fluro = c("#FF0000", "#00FF00", "#0000FF", "#F7941D"),
 
-plot(icamp_blue_discrete)
-
-usethis::use_data(icamp_blue_discrete, overwrite = TRUE)
-
-
-#Colorblind Friendly Palette--------------------------------------------------------
-
-icamp_colorblind_friendly_discrete <- pal_palette(
-
-  colorblind_friendly = c("#000000","#51B4E5", "#D36027", "#999999", "#E5A024", "#009E73", "#F0E442", "#0073B2")#rename this so its not identical to fcn name
+  colorblind_friendly = c("#000000","#51B4E5", "#D36027", "#999999", "#E5A024", "#009E73", "#F0E442", "#0073B2")
 
 )
 
-plot(icamp_colorblind_friendly_discrete)
+plot(icamp_palettes_discrete)
 
-usethis::use_data(icamp_colorblind_friendly_discrete, overwrite = TRUE)
+usethis::use_data(icamp_palettes_discrete, overwrite = TRUE)
 
 #Sequential Color Palettes------------------------------------------------------
 
@@ -34,10 +25,7 @@ icamp_palettes_sequential <- pal_palette(
   green = pal_ramp(pal_color(c("#E1F2E3", "#CDE5D2", "#9CCEA7", "#6CBA7D", "#40AD5A", "#22883B", "#06592A"))),
   bright = pal_ramp(pal_color(c("#FFF3B2", "#FED976", "#FEB24C", "#FD8D3C", "#FC4EA2", "#E31A1C", "#B10026"))),
   diverging_1 = pal_ramp(pal_color(c("#009392", "#39B185", "#9CCB86", "#E9E29C", "#EEB479", "#E88471", "#CF597E"))),
-  diverging_2 = pal_ramp(pal_color(c("#045275", "#089099", "#7CCBA2", "#FCDE9C", "#F0746E", "#DC3977", "#7C1D6F"))),
-  fluro = pal_ramp(pal_color(c("#FF0000", "#00FF00", "#0000FF", "#F7941D")))
-
-
+  diverging_2 = pal_ramp(pal_color(c("#045275", "#089099", "#7CCBA2", "#FCDE9C", "#F0746E", "#DC3977", "#7C1D6F")))
 )
 
 plot(icamp_palettes_sequential)
